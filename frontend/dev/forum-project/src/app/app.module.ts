11f5b963fb7faff6e90899f4d49c96fe1ab241a4
Andrius Sperque <andriussperque@gmail.com>
2018-01-20 18:56:27 -0200

    Initial Home Page component

--- frontend/dev/forum-project/src/app/app.module.ts
+++ frontend/dev/forum-project/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { routing } from "./app.routing";
+import { HomepageComponent } from './home/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     MenuComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
--- frontend/dev/forum-project/src/app/app.routing.ts
+++ frontend/dev/forum-project/src/app/app.routing.ts
@@ -2,12 +2,17 @@ import { Routes, RouterModule } from "@angular/router";
 import { QuestionCreateComponent } from "./forum/question-create/question-create.component";
 import { QuestionDetailComponent } from "./forum/question-detail/question-detail.component";
 import { ForumComponent } from "./forum/forum.component";
+import { HomepageComponent } from "./home/homepage.component";
 
 const APP_ROUTES: Routes = [
     {
         path: '',
-        redirectTo: '/forum',
+        redirectTo: '/home',
         pathMatch: 'full'
+    },
+    {
+        path:'home',
+        component: HomepageComponent
     },
     {
         path: 'forum',
