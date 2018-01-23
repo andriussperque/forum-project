14201f749286bf7da43edcd6e2729e52ca083f33
Andrius Sperque <andriussperque@gmail.com>
2018-01-23 06:34:31 -0200

    remove data for test

--- frontend/dev/forum-project/src/app/forum/question-list/question-list.component.ts
+++ frontend/dev/forum-project/src/app/forum/question-list/question-list.component.ts
@@ -12,14 +12,7 @@ export class QuestionListComponent implements OnInit {
   public questions: Question[] = [
     new Question(1, "How can I implment this system in only two days?","I have a huge software two implement and I only have two days to do that. What should I do?"), 
     new Question(2, "How to ask someone to merry me?","There is any kind of software to ask someone out?"),
-    new Question(3, "How can I implment this system in only two days?","I have a huge software two implement and I only have two days to do that. What should I do?"), 
-    new Question(4, "How can I implment this system in only two days?","I have a huge software two implement and I only have two days to do that. What should I do?"), 
-    new Question(5, "How can I implment this system in only two days?","I have a huge software two implement and I only have two days to do that. What should I do?"), 
-    new Question(6, "How can I implment this system in only two days?","I have a huge software two implement and I only have two days to do that. What should I do?"), 
-    new Question(7, "How can I implment this system in only two days?","I have a huge software two implement and I only have two days to do that. What should I do?"), 
-    new Question(8, "How can I implment this system in only two days?","I have a huge software two implement and I only have two days to do that. What should I do?"), 
-    new Question(9, "How can I implment this system in only two days?","I have a huge software two implement and I only have two days to do that. What should I do?"), 
-    new Question(10, "How can I implment this system in only two days?","I have a huge software two implement and I only have two days to do that. What should I do?")
+    new Question(3, "How can I implment this system in only two days?","I have a huge software two implement and I only have two days to do that. What should I do?")
   ];
 
   constructor(private forumService: ForumService) { }
